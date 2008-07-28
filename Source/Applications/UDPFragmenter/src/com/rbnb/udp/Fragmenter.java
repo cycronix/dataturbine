3d66de1f308e702232510cb212555faa9e247ec7
phubbard <phubbard@794d4657-1347-0410-a318-bdcbb9f8251d>
2008-07-28 21:24:22 +0000

    Added A2 license header, no code changes
    git-svn-id: https://dataturbine.googlecode.com/svn/trunk@156 794d4657-1347-0410-a318-bdcbb9f8251d

--- Source/Applications/UDPFragmenter/src/com/rbnb/udp/Fragmenter.java
+++ Source/Applications/UDPFragmenter/src/com/rbnb/udp/Fragmenter.java
@@ -4,6 +4,18 @@
 	2007/06/05  WHF  Created.
 	2007/06/07  WHF  Extended to allow out-of-order fragments and packets,
 		increasing complexity considerably.	
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
 */
 
 package com.rbnb.udp;
